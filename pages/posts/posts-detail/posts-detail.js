6bd927d092f126318891102dc74566a5288e1153
sam_zhang <353613505@qq.com>
2016-12-28 17:53:16 +0800

    修改不能显示详情的问题

--- pages/posts/posts-detail/posts-detail.js
+++ pages/posts/posts-detail/posts-detail.js
@@ -1,12 +1,17 @@
 var postsData = require('../../../data/posts-data.js')
 Page({
-    onLoad:function(option){
+    onLoad: function (option) {
         var postId = option.id;
+        /*版本更新后后 这种赋值方式已失效
         var postData = postsData.postList[postId];
-        console.log(postData);
+        this.data.postData = postData;
+        //console.log( this.data.postData);
+        */
         //如果在onLoad方法中，不是异步的去执行一个数据绑定
         //则不需要使用this.setData方法
         //只需要对this.data赋值即可实行数据绑定
-        this.data.postData = postData;
+        this.setData({
+            postData: postsData.postList[postId]
+        })
     }
 })
