e81af8d89bcc753eca135a9f6135e42054bd3d12
sam <353613505@qq.com>
2017-01-11 21:26:05 +0800

    11

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ App({
     globalData: {
         g_isPlayingMusic: false,
         g_currentMusicPostId: null
+    },
+    onLaunch: function () {
+        wx.clearStorageSync();
+        console.log("清除缓存");
     }
 })
+
